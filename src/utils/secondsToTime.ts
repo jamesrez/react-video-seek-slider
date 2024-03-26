c36a656d4a0877ce5e03167e4ca2b173c920a1bd
James Rezendes <jamierezendes@gmail.com>
2024-03-25 17:41:57 -0700

    timefix

--- src/utils/secondsToTime.ts
+++ src/utils/secondsToTime.ts
@@ -9,7 +9,7 @@ export function microsecondsToTime(micro: number, offset = 0): Time {
 
   const ms = Math.floor((micro/1000) << 0)
   const mss = Math.floor(micro % 1000)
-  const m = (ms > 0 ? (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms) : "000:").toString();
+  const m = (ms > 0 ? (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms) : "000").toString();
   const s = (mss > 0 ? (mss < 10 ? "00" + mss : mss < 100 ? "0" + mss : mss) : "000").toString();
 
   return {
