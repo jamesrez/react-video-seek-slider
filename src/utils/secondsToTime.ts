cdfd45c1bcb8ffb41eb2eda0d1caceec6bc79a85
James Rezendes <jamierezendes@gmail.com>
2024-03-25 20:42:06 -0700

    seconds included

--- src/utils/secondsToTime.ts
+++ src/utils/secondsToTime.ts
@@ -12,7 +12,7 @@ export function microsecondsToTime(micro: number, offset = 0): Time {
   const ms = Math.floor((micro/1000%1000) << 0)
   const mss = Math.floor(micro % 1000)
   let s = sec > 0 ? ("0" + sec + ":") : "00:";
-  if(sec == 0) s == "";
+  if(sec == 0) s = "";
   const m = (ms > 0 ? (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms) : "000").toString();
   const ss = (mss > 0 ? (mss < 10 ? "00" + mss : mss < 100 ? "0" + mss : mss) : "000").toString();
   
